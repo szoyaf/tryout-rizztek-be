b3cf400f05d794dfde18b5abbe25a1d86b9396b8
szoyaf <shaneyzoyaf@gmail.com>
2025-03-07 15:58:08 +0700

    fix: middleware routing

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,18 +1,9 @@
-import { Hono } from "hono";
 import { jwt } from "hono/jwt";
-import type { JwtVariables } from "hono/jwt";
 
-type Variables = JwtVariables;
-
-const app = new Hono<{ Variables: Variables }>();
-
-app.use(
-  "/auth/*",
-  jwt({
-    secret:
-      process.env.JWT_SECRET ||
-      (() => {
-        throw new Error("JWT_SECRET is not defined");
-      })(),
-  })
-);
+export const authMiddleware = jwt({
+  secret:
+    process.env.JWT_SECRET ||
+    (() => {
+      throw new Error("JWT_SECRET is not defined");
+    })(),
+});
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import { Hono } from 'hono'
+import type { JwtVariables } from "hono/jwt";
 import tryout from './tryout/tryout.route'
 import user from './user/user.route'
 import question from './question/question.route'
 import submission from './submission/submission.route'
 import auth from './auth/auth.route'
+import { authMiddleware } from './auth/auth.middleware';
 
-const app = new Hono()
+type Variables = JwtVariables;
 
-app.route('/auth', auth)
-app.route('/auth/tryout', tryout)
-app.route('/auth/user', user)
-app.route('/auth/question', question)
-app.route('/auth/submission', submission);
+const app = new Hono<{ Variables: Variables }>();
 
-app.get("/", (c) => c.text("Hello World!"));
+app.route('/', auth)
+
+app.use('/api/*', authMiddleware)
+
+app.route('/api/tryout', tryout)
+app.route('/api/user', user)
+app.route('/api/question', question)
+app.route('/api/submission', submission);
 
 export default app
